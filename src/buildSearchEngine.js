ad03964c660e1302c699f6fbd0d2e39d8afef427
yaroslav skiba <109871666+yaroslavskiba@users.noreply.github.com>
2022-10-25 10:33:54 +0300

    sort relevance

--- src/buildSearchEngine.js
+++ src/buildSearchEngine.js
@@ -4,14 +4,45 @@ export default (docs) => ({
     if (!str) {
       return [];
     }
-    const searchReg = str.split(' ').map((cur) => {
+    const searchReg = str.split(' ').map(cur => {
       const cleanToken = cur.replace(/\W/g, '');
       return new RegExp(`\\b${cleanToken}\\b`, 'gi');
     });
-    return docs.map((doc) => ({
+    const sortPrepare =  docs.map((doc) => ({
       ...doc,
       score: searchReg.reduce((acc, word) => acc + (doc.text.match(word)?.length ?? 0), 0),
       count: searchReg.reduce((acc, word) => acc + (word.test(doc.text) ? 1 : 0), 0),
-    })).filter((doc) => doc.score).sort((left, right) => right.score - left.score);
+    }));
+    return sortPrepare.filter((doc) => doc.score || doc.count).sort((left, right) => right.count - left.count || right.score - left.score,
+  );
   },
 });
+
+// const buildSearchEngine = (docs) => ({
+//   docs,
+//   search: (str) => {
+//     if (!str) {
+//       return [];
+//     }
+//     const searchReg = str.split(' ').map(cur => {
+//       const cleanToken = cur.replace(/\W/g, '');
+//       return new RegExp(`\\b${cleanToken}\\b`, 'gi');
+//     });
+//     const sortPrepare =  docs.map((doc) => ({
+//       ...doc,
+//       score: searchReg.reduce((acc, word) => acc + (doc.text.match(word)?.length ?? 0), 0),
+//       count: searchReg.reduce((acc, word) => acc + (word.test(doc.text) ? 1 : 0), 0),
+//     }));
+//     return sortPrepare.filter((doc) => doc.score || doc.count).sort((left, right) => right.count - left.count || right.score - left.score,
+//   );
+//   },
+// });
+
+// const doc1 = { id: 'doc1', text: "I can't shoot straight unless I've had a pint!" };
+// const doc2 = { id: 'doc2', text: "Don't shoot shoot shoot that thing at me." };
+// const doc3 = { id: 'doc3', text: "I'm your shooter." };
+// const docs = [doc1, doc2, doc3];
+
+// const searchEngine = buildSearchEngine(docs);
+
+// searchEngine.search('shoot me?'); // ['doc2', 'doc1']
