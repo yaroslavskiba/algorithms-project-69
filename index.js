684265a8008f222b9de604c1dc99aa78b41c1738
yaroslavs skiba <yaroslavskiba322@gmail.com>
2022-10-27 00:15:15 +0300

    wtg? fix

--- index.js
+++ index.js
@@ -2,15 +2,4 @@
 
 import buildSearchEngine from './src/buildSearchEngine.js';
 
-const doc1 = { id: 'doc1', text: "I can't shoot straight unless I've had a pint!" };
-const doc2 = { id: 'doc2', text: "Don't shoot shoot shoot that thing at me." };
-const doc3 = { id: 'doc3', text: "I'm your shooter." };
-const docs = [doc1, doc2, doc3];
-
-const searchEngine = buildSearchEngine(docs);
-const result = searchEngine.search('shoot at me');
-
-console.log(doc1);
-console.log(doc2);
-console.log(doc3);
-console.log(result);
+export default buildSearchEngine;
